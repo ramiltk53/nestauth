866fb0bd17e718576795b6b9ab517c472527a2e6
ramiltk53 <ramiltk53@gmail.com>
2021-12-30 22:00:59 +0300

    everything is ready except the database

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
+  ],
+})
+export class AppModule {}
